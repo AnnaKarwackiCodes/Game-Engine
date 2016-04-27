f9e5e8585e28108789eeb511a3da8fc928b729c3
Anna Karwacki <avk9238@ad.rit.edu>
2016-04-27 12:41:56 -0400

    Camera work
    v1.0

--- THE WORKING ONE/WindowDemo/Engine.cpp
+++ THE WORKING ONE/WindowDemo/Engine.cpp
@@ -36,7 +36,7 @@ bool Engine::init()
 	}
 
 	//create a windowed mode window
-	GLFWwindowPtr = glfwCreateWindow(900, 600, "Anna Karwacki DSA1 Shader", NULL, NULL);
+	GLFWwindowPtr = glfwCreateWindow(900, 600, "Anna Karwacki DSA1 Engine", NULL, NULL);
 
 	//Make it the currently active window, or quit
 	if (GLFWwindowPtr != nullptr)
@@ -66,7 +66,6 @@ bool Engine::gameLoop()
 {
 	currTime = glfwGetTime();
 	prevFrame = currTime;
-	Model mods[3] = { mod1,mod2,mod3 };
 	// loop until the user closes the window
 	while (!glfwWindowShouldClose(GLFWwindowPtr))
 	{
@@ -123,6 +122,30 @@ bool Engine::gameLoop()
 		{
 			mod2.TextureChange("door.jpg");
 		}
+
+		//move or turn camera
+		glm::vec3 camLoc = { 0,0,2 };
+		glm::vec3 camRot = { 0,0,0 };
+
+		glm::mat3 rotMat = (glm::mat3)glm::yawPitchRoll(camRot.y, camRot.x, camRot.z);
+
+		glm::vec3 eye = camLoc;
+		glm::vec3 center = eye + rotMat * glm::vec3(0, 0, -1);
+		glm::vec3 up = rotMat * glm::vec3(0, 1, 0);
+
+		glm::mat4 lookAtMat = glm::lookAt(eye, center, up);
+
+		// zoom or change aspect ratio
+		float zoom = 1.f;
+		int width = 800;
+		int height = 600;
+
+		float fovy = 3.14159f * .4f / zoom;
+		float aspect = (float)width / (float)height;
+		float zNear = .01f;
+		float zFar = 1000.f;
+
+		glm::mat4 perspectiveMat = glm::perspective(fovy, aspect, zNear, zFar);
 
 		mod1.render();
 		mod2.render();
--- THE WORKING ONE/WindowDemo/Texture.cpp
+++ THE WORKING ONE/WindowDemo/Texture.cpp
@@ -33,9 +33,6 @@ bool Texture::loadTexture(char* name)
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_SRGB_ALPHA, FreeImage_GetWidth(image32bit), FreeImage_GetHeight(image32bit), 0, GL_BGRA, GL_UNSIGNED_BYTE, (void*)FreeImage_GetBits(image32bit));
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
-	//textures.push_back(texID);
-	//texIndex = textures.size();
-
 	FreeImage_Unload(image32bit);
 	glBindTexture(GL_TEXTURE_2D, 0);
 	return true;
